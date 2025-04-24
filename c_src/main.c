f0adf3fea3c67025425fdd253a350d9b682b67fa
Josh CH Yeh <jy0825@bu.edu>
2025-04-24 13:48:00 -0400

    Update main.c

--- c_src/main.c
+++ c_src/main.c
@@ -128,7 +128,7 @@ void render(void) {
     SDL_Flip(screen);
 }
 
-int main(int argc, char* argv[]) {
+int main(void) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
         return 1;
